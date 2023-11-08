abf0ae68cfd870d6043adad9355de4bfd468a9e9
Benny Reyes <benny.reyes@walmart.com>
2023-11-08 00:11:25 -0600

    Initial Commit

---
+++ ManejoErrores/ManejoErroresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManejoErroresApp.swift
+//  ManejoErrores
+//
+//  Created by Benny Reyes on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ManejoErroresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
